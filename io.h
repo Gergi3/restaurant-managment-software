833ec01108ebb2213415cdbe25b29c41e443a574
Georgi Petrov <hailanimepenchou@gmail.com>
2025-01-12 23:10:03 +0200

    🔨 Add IO service

---
+++ io.h
@@ -0,0 +1,13 @@
+#pragma once
+
+void input(
+	char* result,
+	unsigned maxInputChars = 1024);
+
+void print(
+	const char const* toPrint,
+	unsigned newLinesCount = 1);
+
+void printNewLines(unsigned count = 1);
+
+void printNewLine();
