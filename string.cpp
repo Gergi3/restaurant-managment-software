cd2a80f95599ecc5d6465d7040424f9d5b6cc2c7
Georgi Petrov <hailanimepenchou@gmail.com>
2025-01-12 23:07:46 +0200

    ✨ Add trims, shifts, counts, length to string service

--- string.cpp
+++ string.cpp
@@ -1,5 +1,113 @@
 #include "string.h";
 
+unsigned length(const char const* str)
+{
+	if (!str)
+	{
+		return 0;
+	}
+
+	unsigned counter = 0;
+	while (*str)
+	{
+		counter++;
+		str++;
+	}
+
+	return counter;
+}
+
+unsigned countSuffixChars(const char const* str, char ch)
+{
+	if (!str)
+	{
+		return 0;
+	}
+
+	unsigned count = 0;
+	while (*str == ch)
+	{
+		count++;
+		str++;
+	}
+
+	return count;
+}
+
+unsigned countPrefixChars(const char const* str, char ch)
+{
+	if (!str)
+	{
+		return 0;
+	}
+
+	str += length(str) - 1;
+	
+	unsigned count = 0;
+	while (*str == ch)
+	{
+		count++;
+		str--;
+	}
+
+	return count;
+}
+
+void shiftLeftMutate(char* str, unsigned pos)
+{
+	if (!str)
+	{
+		return;
+	}
+
+	unsigned rhs = pos;
+	unsigned lhs = 0;
+
+	while (str[rhs])
+	{
+		str[lhs] = str[rhs];
+		rhs++;
+		lhs++;
+	}
+
+	str[lhs] = '\0';
+}
+
+void trimLeftMutate(char* str, char ch)
+{
+	if (!str)
+	{
+		return;
+	}
+
+	unsigned counter = countSuffixChars(str, ch);
+	shiftLeftMutate(str, counter);
+}
+
+void trimRightMutate(char* str, char ch)
+{
+	if (!str)
+	{
+		return;
+	}
+
+	unsigned counter = countPrefixChars(str, ch);
+	unsigned strLen = length(str);
+
+	str[strLen - counter] = '\0';
+}
+
+void trimMutate(char* str, char ch)
+{
+	if (!str)
+	{
+		return;
+	}
+
+	trimLeftMutate(str, ch);
+	trimRightMutate(str, ch);
+}
+
 char toLower(char ch)
 {
 	if (ch >= 'A' && ch <= 'Z')
