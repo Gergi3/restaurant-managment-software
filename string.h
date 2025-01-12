9c93763cc0d6c64c14680b41cf983d3b29cd61d1
Georgi Petrov <hailanimepenchou@gmail.com>
2025-01-12 23:07:46 +0200

    ✨ Added trims, shifts, counts, length to string service

--- string.h
+++ string.h
@@ -1,5 +1,19 @@
 #pragma once
 
+unsigned length(const char const* str);
+
+unsigned countSuffixChars(const char const* str, char ch);
+
+unsigned countPrefixChars(const char const* str, char ch);
+
+void shiftLeftMutate(char* str, unsigned pos = 1);
+
+void trimLeftMutate(char* str, char ch = ' ');
+
+void trimRightMutate(char* str, char ch = ' ');
+
+void trimMutate(char* str, char ch = ' ');
+
 char toLower(char ch);
 
 bool contains(
