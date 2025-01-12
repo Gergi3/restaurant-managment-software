7a4ed9f9cd7a51eede3d06f5ab7213091cd78670
Georgi Petrov <hailanimepenchou@gmail.com>
2025-01-12 23:12:11 +0200

    🔨 Added user service for choosing a role

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include "system.h"
+#include "string.h"
+#include "user.h"
 
 int main()
 {
-	initializeSystem();
+	bool systemIsUp = initializeSystem();
+
+	if (!systemIsUp)
+	{
+		return -1;
+	}
+
+	Role role = getRole();
 }
