18e42ca7f05c107001da9416a5f5e654558ce5d6
Georgi Petrov <hailanimepenchou@gmail.com>
2025-01-16 01:33:05 +0200

    ✨ Add panel constants

---
+++ panelConstants.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace PANEL_CONSTANTS
+{
+	constexpr unsigned MIN_SERVER_OPTION = 1;
+	constexpr unsigned MIN_MANAGER_OPTION = 1;
+	constexpr unsigned MAX_SERVER_OPTION = 7;
+	constexpr unsigned MAX_MANAGER_OPTION = 15;
+}
