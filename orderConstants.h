128bc0dc19592e152287583e04c899a55c5c9e7d
Georgi Petrov <hailanimepenchou@gmail.com>
2025-01-16 21:02:28 +0200

    ✨ Add order item and order constants

--- orderConstants.h
+++ orderConstants.h
@@ -2,5 +2,16 @@
 
 namespace ORDER_CONSTANTS
 {
-	constexpr char const* FILE_NAME = "order.txt";
+	constexpr char const* FILE_NAME = "orders.txt";
+
+	constexpr unsigned MENU_ITEM_NOT_FOUND_FAIL_CODE = 300;
+	constexpr char const* MENU_ITEM_NOT_FOUND_FAIL_MESSAGE = 
+		"The order was cancelled because no such menu item was found.";
+
+	constexpr unsigned REMOVAL_FAIL_CODE = 301;
+	constexpr char const* REMOVAL_FAIL_MESSAGE =
+		"The order was not cancelled because no such order was found.";
+
+	constexpr unsigned FAIL_CODE_START = 300;
+	constexpr unsigned FAIL_CODE_END = 399;
 }
---
+++ orderItem.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "menuConstants.h";
+
+struct OrderItem
+{
+	unsigned id = 0;
+	char menuItemName[MENU_CONSTANTS::MAX_NAME_LENGTH + 1];
+};
