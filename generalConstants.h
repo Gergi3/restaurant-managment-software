449eeece5781e4133c806be0f068c556ead0b602
Georgi Petrov <hailanimepenchou@gmail.com>
2025-01-16 21:06:52 +0200

    ✨ Add max uint digits count to general constants

--- generalConstants.h
+++ generalConstants.h
@@ -22,4 +22,5 @@ namespace GENERAL_CONSTANTS
 	constexpr unsigned FAIL_CODES_LENGTH = 10;
 	constexpr unsigned EMPTY_CODE = 0;
 	constexpr unsigned INPUT_BUFFER_SIZE = 1024;
+	constexpr unsigned MAX_UINT_DIGITS_COUNT = 10;
 }
