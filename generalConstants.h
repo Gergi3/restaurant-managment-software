0f6288da7e67d1c148cdf74a898517192f5e3463
Georgi Petrov <hailanimepenchou@gmail.com>
2025-01-16 02:15:30 +0200

    🚚 Move files count constant from system service to general constants

--- generalConstants.h
+++ generalConstants.h
@@ -2,6 +2,7 @@
 
 namespace GENERAL_CONSTANTS
 {
+	constexpr unsigned FILES_COUNT = 4;
 	constexpr unsigned FAIL_CODES_LENGTH = 10;
 	constexpr unsigned EMPTY_CODE = 0;
 	constexpr unsigned INPUT_BUFFER_SIZE = 1024;
--- system.cpp
+++ system.cpp
@@ -1,8 +1,8 @@
+#include "generalConstants.h"
 #include "system.h"
 #include <fstream>
 
-const unsigned char FILES_COUNT = 4;
-const char const* FILE_NAMES[FILES_COUNT] =
+const char const* FILE_NAMES[GENERAL_CONSTANTS::FILES_COUNT] =
 {
 	"orders.txt",
 	"menu.txt",
@@ -21,7 +21,7 @@ bool initializeSystemFiles()
 {
 	std::ofstream writer;
 
-	for (size_t i = 0; i < FILES_COUNT; i++)
+	for (size_t i = 0; i < GENERAL_CONSTANTS::FILES_COUNT; i++)
 	{
 		const char const* fileName = FILE_NAMES[i];
 		writer.open(fileName, std::ios::app);
